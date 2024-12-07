a8265912660628be860bf56d43ab66b3a529678e
porporpor020202 <porporpor020202@gmail.com>
2024-12-07 19:44:30 +0900

    first init

---
+++ src/main/java/me/practice/springbootblogpractice/SpringBootBlogPracticeApplication.java
@@ -0,0 +1,13 @@
+package me.practice.springbootblogpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlogPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBlogPracticeApplication.class, args);
+	}
+
+}
